ecfee3aafc19ec748bdb866e129511d8d56c1aa4
alex-no <alex@4n.com.ua>
2025-03-05 04:10:49 +0200

    test DB

---
+++ www_app/app/Http/Controllers/DatabaseController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseController extends Controller
+{
+    public function checkDatabaseConnection()
+    {
+        try {
+            // Проверяем соединение
+            DB::connection()->getPdo();
+
+            // Получаем список таблиц
+            $tables = DB::select('SHOW TABLES');
+            // $tables = DB::select("SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%'");
+
+            return response()->json([
+                'message' => 'Database connection is successful',
+                'tables' => array_map('current', $tables) // Преобразуем результат в простой массив
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Could not connect to the database. Please check your configuration.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+}
--- www_app/routes/web.php
+++ www_app/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DatabaseController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/check-db', [DatabaseController::class, 'checkDatabaseConnection']);
