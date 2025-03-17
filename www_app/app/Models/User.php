23f6b7431d0ebca21f2fba22aef97b689a41675b
alex-no <alex@4n.com.ua>
2025-03-17 04:02:27 +0200

    1

--- www_app/app/Models/User.php
+++ www_app/app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -18,7 +18,7 @@ use Laravel\Sanctum\HasApiTokens;
  *     @OA\Property(property="email", type="string", example="user@example.com")
  * )
  */
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens, HasFactory, Notifiable;
