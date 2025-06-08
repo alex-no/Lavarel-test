aeb4986990a1a5ea4915ca1c723cc36ab9b5c39e
alex-no <alex@4n.com.ua>
2025-06-09 02:40:30 +0300

    fix

--- www_app/app/Http/Controllers/Api/PaymentController.php
+++ www_app/app/Http/Controllers/Api/PaymentController.php
@@ -113,8 +113,9 @@ class PaymentController extends Controller
             $orderId = 'ORD-' . now()->format('Ymd-His') . '-' . Str::random(6);
 
             $order = Order::create([
-                'user_id'     => $user->id,
-                'order_id'    => $orderId,
+                'user_id'  => $user->id,
+                'order_id' => $orderId,
+                'amount'   => $request->amount,
             ]);
         } else {
             $order = Order::where('order_id', $orderId)->first();
